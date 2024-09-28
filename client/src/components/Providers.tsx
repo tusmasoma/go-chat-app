6b782d9f3d5038bda8d4516545072d13de6fe8fd
tusmasoma <s.k18@icloud.com>
2024-09-28 22:37:09 +0900

    chore: delete

--- client/src/components/Providers.tsx
+++
@@ -1,6 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import { NextUIProvider } from '@nextui-org/react';
-
-export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
-};
--- client/src/types/Message.tsx
+++
@@ -1,5 +0,0 @@
-export interface Message {
-    id: string;
-    content: string;
-    role: 'user' | 'system' | 'assistant'; // roleに変更
-}
